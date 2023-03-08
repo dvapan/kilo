7c3806ea7200e0209b418e2cdad66cf6802b92d7
Danil Apanovich <dvapan@gmail.com>
2023-03-08 19:50:52 +0600

    clear screen

--- kilo.c
+++ kilo.c
@@ -48,6 +48,12 @@ char editor_read_key() {
     }
     return c;
 }
+
+/*** output ***/
+void editor_refresh_screen() {
+    write(STDOUT_FILENO, "\x1b[2J",4);
+}
+
 /*** input ***/
 
 void editor_process_keypress() {
@@ -66,6 +72,7 @@ int main() {
     enable_raw_mode();
 
     while(1) {
+        editor_refresh_screen();
         editor_process_keypress();
     }
 
