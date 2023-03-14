acdb69198ea68d6a23f546df1c17919a78c494b6
Danil Apanovich <dvapan@gmail.com>
2023-03-14 23:29:31 +0600

    Line clears one at time

--- kilo.c
+++ kilo.c
@@ -121,6 +121,7 @@ void editor_draw_rows(struct abuf *ab) {
     for(y = 0; y < E.screenrows; y++) {
         ab_append(ab, "~", 1);
 
+	ab_append(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1) {
             ab_append(ab, "\r\n", 2);
         }
@@ -131,7 +132,6 @@ void editor_refresh_screen() {
     struct abuf ab = ABUF_INIT;
 
     ab_append(&ab, "\x1b[?25l",6);
-    ab_append(&ab, "\x1b[2J", 4);
     ab_append(&ab, "\x1b[H", 3);
 
     editor_draw_rows(&ab);
