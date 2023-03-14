bef3fb9a44e286c0b5e2706ab9f9a67527326c1b
Danil Apanovich <dvapan@gmail.com>
2023-03-14 22:49:59 +0600

    Window size, easy

--- kilo.c
+++ kilo.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -12,6 +13,8 @@
 /*** data ***/
 
 struct editor_config {
+    int screenrows;
+    int screencols;
     struct termios orig_termios;
 };
 
@@ -56,11 +59,23 @@ char editor_read_key() {
     return c;
 }
 
+int get_window_size(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /*** output ***/
 
 void editor_draw_rows() {
     int y;
-    for(y = 0; y < 24; y++) {
+    for(y = 0; y < E.screenrows; y++) {
         write(STDIN_FILENO, "~\r\n", 3);
     }
 }
@@ -90,8 +105,14 @@ void editor_process_keypress() {
 
 /*** init ***/
 
+void init_editor() {
+    if (get_window_size(&E.screenrows, &E.screencols) == -1) 
+        die("get_window_size");
+}
+
 int main() {
     enable_raw_mode();
+    init_editor();
 
     while(1) {
         editor_refresh_screen();
